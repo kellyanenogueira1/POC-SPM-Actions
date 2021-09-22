7ebae3ad66eb1bc3060a36f4e0aab11a449a30ae
MariaKellyane <kellyane.nogueira@popcode.com.br>
2021-09-22 15:14:22 -0300

    Add spm dependence

---
+++ POC-SPM-Actions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POC-SPM-Actions
+//
+//  Created by Kellyane Nogueira on 22/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
